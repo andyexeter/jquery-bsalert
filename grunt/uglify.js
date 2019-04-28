9ee0b16073e5373625b656ae392b585e1155de33
andyexeter <palmer.andy@gmail.com>
2019-04-28 12:04:02 +0100

    Preserve license in minified file

--- grunt/uglify.js
+++ grunt/uglify.js
@@ -2,10 +2,12 @@ module.exports = {
     dist: {
         options: {
             sourceMap: false,
-            report: "gzip",
-            preserveComments: /(?:^!|@(?:license|preserve|cc_on))/
+            report: 'gzip',
+            output: {
+                comments: /(?:^!|@(?:license|preserve|cc_on))/
+            }
         },
-        src: "<%= files.main %>",
-        dest: "<%= files.min %>"
+        src: '<%= files.main %>',
+        dest: '<%= files.min %>'
     }
 };
